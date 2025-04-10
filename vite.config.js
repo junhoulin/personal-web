29ecc0d5a096659543011f67fdffa80bf3f4cf53
JUNHOULIN <yy6313531@gmail.com>
2025-04-10 16:09:55 +0800

    修正html.index

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-  base: "/personal-web/", // 替换为你的仓库名
+  base: '/personal-web/' // 替换为你的仓库名
 });
